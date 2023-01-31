adfd24634b7d78e9f43c9d04560207781f098398
pujaKundu <iampujakundu@gmail.com>
2023-01-31 13:26:11 +0600

    banner and brand

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from "react";
+import { google, slack, atlassian, dropbox, shopify } from "./imports";
+import "./Brand.css";
 
-const Brand = () => {
-  return (
-    <div>Brand</div>
-  )
-}
+const Brand = () => (
+  <div className="gpt3__brand-names_container section__padding">
+    <div>
+      <img src={google} />
+    </div>
+    <div>
+      <img src={slack} />
+    </div>
+    <div>
+      <img src={atlassian} />
+    </div>
+    <div>
+      <img src={dropbox} />
+    </div>
+    <div>
+      <img src={shopify} />
+    </div>
+  </div>
+);
 
-export default Brand
+export default Brand;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,8 @@
 import React, { useState } from "react";
 import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
 import logo from "../../assets/logo.svg";
+import Menu from "./Menu";
 import "./Navbar.css";
-
-const Menu = () => (
-  <>
-    <p>
-      <a href="#href">Home</a>
-    </p>
-    <p>
-      <a href="#wgpt3">What is GPT?</a>
-    </p>
-    <p>
-      <a href="#possibility">Open AI</a>
-    </p>
-    <p>
-      <a href="#features">Case Studies</a>
-    </p>
-    <p>
-      <a href="#blog">Library</a>
-    </p>
-  </>
-);
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);

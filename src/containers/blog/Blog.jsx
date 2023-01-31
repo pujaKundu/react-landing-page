83309804ad4ca43fffd672bfe3dbb902db5de77a
pujaKundu <iampujakundu@gmail.com>
2023-01-31 15:31:05 +0600

    get started

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import React from 'react';
+import Article from '../../components/article/Article';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
+import './Blog.css';
 
-const Blog = () => {
-  return (
-    <div>Blog</div>
-  )
-}
+let currentDate = new Date().toJSON().slice(0, 10);
 
-export default Blog
+const Blog = () => (
+  <div className="gpt3__blog section__padding" id="blog">
+    <div className="gpt3__blog-heading">
+      <h1 className="gradient__text">
+        A lot is happening, <br /> We are blogging about it.
+      </h1>
+    </div>
+    <div className="gpt3__blog-container">
+      <div className="gpt3__blog-container_groupA">
+        <Article
+          imgUrl={blog01}
+          date={currentDate}
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+      </div>
+      <div className="gpt3__blog-container_groupB">
+        <Article
+          imgUrl={blog02}
+          date={currentDate}
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog03}
+          date={currentDate}
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog04}
+          date={currentDate}
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+        <Article
+          imgUrl={blog05}
+          date={currentDate}
+          text="GPT-3 and Open  AI is the future. Let us exlore how it is?"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default Blog;

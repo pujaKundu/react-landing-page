bba1d7a05b9b411de0c98cfe79fb079f37718024
pujaKundu <iampujakundu@gmail.com>
2023-01-31 12:59:27 +0600

    navbar responsive

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,29 +1,75 @@
-import React from "react";
-import {RiMenu3Line, RiCloseLine} from 'react-icons/ri'
-import logo from '../../assets/logo.svg'
-import './Navbar.css'
+import React, { useState } from "react";
+import { RiMenu3Line, RiCloseLine } from "react-icons/ri";
+import logo from "../../assets/logo.svg";
+import "./Navbar.css";
+
+const Menu = () => (
+  <>
+    <p>
+      <a href="#href">Home</a>
+    </p>
+    <p>
+      <a href="#wgpt3">What is GPT?</a>
+    </p>
+    <p>
+      <a href="#possibility">Open AI</a>
+    </p>
+    <p>
+      <a href="#features">Case Studies</a>
+    </p>
+    <p>
+      <a href="#blog">Library</a>
+    </p>
+  </>
+);
 
 const Navbar = () => {
-  return <div className='gpt3__navbar'>
-    <div className="gpt3__navbar-links">
-      <div className="gpt3__navbar-links_logo">
-        <img src={logo} alt="logo" />
+  const [toggle, setToggle] = useState(false);
+
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+        {/* navigation links */}
+        <div className="gpt3__navbar-links_container">
+          <Menu />
+        </div>
+        <div className="gpt3__navbar-sign">
+          <p>Sign in</p>
+          <button type="button">Sign up</button>
+        </div>
       </div>
-      {/* navigation links */}
-      <div className="gpt3__navbar-links_container">
-        <p><a href="#href">Home</a></p>
-        <p><a href="#wgpt3">What is GPT?</a></p>
-        <p><a href="#possibility">Open AI</a></p>
-        <p><a href="#features">Case Studies</a></p>
-        <p><a href="#blog">Library</a></p>
-      </div>
-      <div className="gpt3__navbar-btn_container">
-        <p>Sign in</p>
-        <button type="button">Sign up</button>
+      {/* mobile version */}
+      <div className="gpt3__navbar-menu">
+        {toggle ? (
+          <RiCloseLine
+            color="#FFFFFF"
+            size={25}
+            onClick={() => setToggle(false)}
+          />
+        ) : (
+          <RiMenu3Line
+            color="#FFFFFF"
+            size={25}
+            onClick={() => setToggle(true)}
+          />
+        )}
+        {toggle && (
+          <div className="gpt3__navbar-menu_container scale-up-center">
+            <div className="gpt3__navbar-menu_container-links">
+              <Menu />
+              <div className="gpt3__navbar-menu_container-links-sign">
+                <p>Sign in</p>
+                <button type="button">Sign up</button>
+              </div>
+            </div>
+          </div>
+        )}
       </div>
     </div>
-    </div>;
+  );
 };
 
 export default Navbar;
- 
